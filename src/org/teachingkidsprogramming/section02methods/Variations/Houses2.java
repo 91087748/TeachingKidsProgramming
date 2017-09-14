63a9703dde81b5db87a20499f32f1892e1405391
91087748 <91087748@mvusd.net>
2017-09-14 14:31:22 -0700

    houses

--- src/org/teachingkidsprogramming/section02methods/Variations/Houses2.java
+++ src/org/teachingkidsprogramming/section02methods/Variations/Houses2.java
@@ -1,9 +1,9 @@
-package org.teachingkidsprogramming.section02methods;
+package org.teachingkidsprogramming.section02methods.Variations;
 
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
 
-public class Houses
+public class Houses2
 {
   public static void main(String[] args)
   {
@@ -20,12 +20,26 @@ public class Houses
   {
     Tortoise.setPenColor(Reds.Red);
     Tortoise.move(height);
-    Tortoise.turn(90);
-    Tortoise.move(30);
-    Tortoise.turn(90);
+    //drawFlatRoof();
+    // draw a pointy roof
+    pointyroof();
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
     Tortoise.turn(-90);
+  }
+  private static void pointyroof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(15);
+    Tortoise.turn(90);
+    Tortoise.move(15);
+    Tortoise.turn(45);
+  }
+  private static void drawFlatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
   }
 }
